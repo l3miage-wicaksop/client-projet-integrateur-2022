75e87ea8efb812c442d06933772c57e1acaa7fc7
Nurbek <ns490367@gmail.com>
2022-04-25 11:55:25 +0200

    Dima

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,36 @@
 import { Component } from '@angular/core';
 import { circle, latLng, Layer, MapOptions, marker, tileLayer } from 'leaflet';
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDWsoddNHtY-Rwirp-jyHB9-0tzhvT6coE",
+
+  authDomain: "projetintegrateur-2ed35.firebaseapp.com",
+
+  projectId: "projetintegrateur-2ed35",
+
+  storageBucket: "projetintegrateur-2ed35.appspot.com",
+
+  messagingSenderId: "62423672571",
+
+  appId: "1:62423672571:web:86c95b9c2f6f317145b25e"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
 
 @Component({
   selector: 'app-root',
