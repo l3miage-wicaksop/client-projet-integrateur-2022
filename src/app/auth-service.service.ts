d3efe994138ce72fb434e2e33c6ee55afa110b30
Nurbek <68291178+Serzhanov@users.noreply.github.com>
2022-04-27 02:56:07 +0300

    Nurbek created branch

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient,HttpHeaders} from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Chamis,Chami } from './iterfaces';
-import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
